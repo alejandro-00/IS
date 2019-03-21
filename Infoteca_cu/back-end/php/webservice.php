6ebb10001f3faaebc38b93d2d771cffd911e4f87
Alejandro <alexnengveerav@gmail.com>
2019-03-21 01:23:27 -0600

    .

--- Infoteca_cu/back-end/php/webservice.php
+++ Infoteca_cu/back-end/php/webservice.php
@@ -1,3 +1,15 @@
 <?php
-include 'conection.php';
+include 'connection.php';
+
+
+$results;
+$MAT = $_GET["matricula"];
+
+$stbi = $conn->prepare("CALL ALM(?)");
+$stbi->bindParam(1, $MAT);
+$stbi->setFetchMode(PDO::FETCH_ASSOC);
+$stbi->execute();
+$results = $stbi->fetchAll();
+
+print_r($results);
 ?>
