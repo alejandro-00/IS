a11936633ddae6cd315a5a98d43487761b7e7ae2
Alejandro <alexnengveerav@gmail.com>
2019-04-15 21:43:53 -0500

    free pdf generator

---
+++ Infoteca_cu/back-end/php/text2.php
@@ -0,0 +1,42 @@
+<?php
+require('fpdf.php');
+//include 'connection.php';
+
+//se crea la clase PDF eredando FPDF para editar el Header y el Footer
+class PDf extends FPDF
+{
+    //funcion que crea la cabecera del archivo
+    function Header(){
+        /*se inserta una imagen (Nombre/ruta del archivo,posicion en x,posicion en y, algo,ancho,formato de archivo)
+        posicion x & y son respecto al de las coordenadas de la página*/
+        $this->Image('Escudo-240x300.png',10,8,24,30);
+        /*se selecciona un tipo y tamaño de fuente (se ecuentran en la carpeta font)
+        (Nombre,estilo,tamaño)*/
+        $this->SetFont('Arial','',8);
+        $txt="Universidad Aau\xF3noma de Coahuila\nSistema se Infotecas Centrales\nAtenci\xF3n al Usuario\n\nRegistro diario de informaci\xF3n y paqueter\xEDa \n mes-a\xF1o";
+        /*Se utiliza un objeto multicelda que nos permite hacer saltos de linea. 
+        Por cada linea crea una celda (ancho,alto,string,borde[0 ó 1],alineacion)
+        el cero en el ancho indica que ocupe el ancho disponible, no aplica en la altura*/
+        $this->MultiCell(0,4.5,$txt,1,'C');
+    }
+
+    function Footer(){
+        /*SetY posiciona el Footer a una distancia (mm) del origen de la pagina.
+        El signo (-) indica que empieza de abajo hacia arriba*/
+        $this->SetY(-10);
+        $this->SetFont('Arial','',8);
+        $this->Cell(0,10,"SI-F-AUS-02  Revisi\xF3n 1 - " . date('d-m-y'),0,0);
+    }
+}
+
+/*PDF(horientacion,medida,formato del papel) 
+Horientacion: P = portrait ; L = Landscape
+Medida: pt,mm,cm,in (pt = punto; un punto es igual a 1/72 de pulgada, 0.35 mm aprox)
+Formato: A4, A3, A5, Letter, Legal.
+*/
+$pdf = new PDF('L','mm','Legal');
+
+
+$pdf->SetTitle("Reporte 1");
+$pdf->AddPage();
+$pdf->Output();
--- Infoteca_cu/back-end/php/wsregistro.php
+++ Infoteca_cu/back-end/php/wsregistro.php
@@ -3,7 +3,7 @@ include 'connection.php';
 
 //recibe json {MAT INT,NA STRING,AP STRING,AM STRING,NC STRING,NF STRING,LL INT,T INT}
 $json=json_decode($_GET["ALUMNO"],false);
-$results;
+$result[];
 try{
     $stbi = $conn->prepare("CALL registrar(?,?,?,?,?,?,?,?)");
     $stbi->bindParam(1,$json->MAT);
@@ -14,19 +14,20 @@ try{
     $stbi->bindParam(6,$json->NF);
     $sbti->bindParam(7,$json->LL);
     $stbi->bindParam(8,$json->T);
+    //*investigar datos de execute()*//
     $stbi->execute();
     //Comprueba si coincide la matrícula del último registro con a matrícula del alumno
     if($json->MAT == $conn->lastInserId()){
         //if true
-        echo 1;
-    }
-    else{
-        //if false
-        echo 0;
+        $result[0]=1;
+        $result[1]="Registro exitoso";
+        echo $json=json_encode($result[])
     }
 }
 catch(PDOException $e){
-    echo $e->getMessage();
+    $result[0]=0;
+    $result[1]=$e->getMessage();
+    echo $json=json_encode($result);
 }
 
 $conn=null;
