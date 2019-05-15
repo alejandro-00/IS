c856892a0f0bf30ad4ad4077b5e304a2eff96504
Mario Alejandro Manzano Montes <44003981+mariomanza@users.noreply.github.com>
2019-05-14 23:00:05 -0500

    Update externos.php

--- Infoteca_cu/back-end/php/externos.php
+++ Infoteca_cu/back-end/php/externos.php
@@ -9,10 +9,11 @@ if($_SESSION['userid']=='' or $_SESSION['pswd']==''){
 $data = json_decode($_GET);
 
 try {
-    $stbi = $conn->prepare("INSERT INTO tabla (matricula, fecha, hora) VALUES (?,?,?)");
+    $stbi = $conn->prepare("INSERT INTO tabla (nombre,ap,am,ocup,tel,inst, fecha, hora) VALUES (?,?,?)");
     $stbi->setFetchMode(PDO::FETCH_ASSOC);
-    $stbi->execute([$data['mat'], $date, $time]);
+    $stbi->execute([$data['nombre'],$data['ap'],$data['am'],$data['ocup'],$data['tel'],$data['inst'], $date, $time]);
     $stbi = null;
+    echo true;
 } catch (Exception $e) {
     echo 'Message: '.$e->getMessage();
 }
