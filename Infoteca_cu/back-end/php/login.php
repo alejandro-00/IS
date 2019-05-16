97498f1bed9131586c0147f6a9066ed5a34c9468
Alejandro <alexnengveerav@gmail.com>
2019-05-16 05:58:08 -0500

    se implemento el inicio de sesion

--- Infoteca_cu/back-end/php/login.php
+++ Infoteca_cu/back-end/php/login.php
@@ -3,26 +3,27 @@ include 'connection.php';
 session_start();
 
 $data = json_decode($_POST["x"]);
-
+$user=[];
+$user[0]=$data->name;
+$user[1]=$data->pswd;
 try {
-    $stbi = $conn->prepare("Select NOM_USER, PASW_USER, permissions from users where name = ?");
+    $stbi = $conn->prepare("SELECT ID_PERFIL,NOM_USER,PASW_USER FROM users WHERE NOM_USER='$user[0]' AND PASW_USER='$user[1]';");
     $stbi->setFetchMode(PDO::FETCH_ASSOC);
-    $stbi->execute($data['name']);
-    $res = $stbi.fetch();
-    if ($data['pswd']==$res['pswd']) {
-		$_SESSION['userid'] = $data['name'];
-		$_SESSION['pswd'] = $data['pswd'];
-        $_SESSION['status'] = $res['permissions'];
-		$stbi = null;
-		if ($res['permissions']==1) {
-            echo 'admin';
-        } elseif ($res['permissions']==3) {
-            echo 'registro';
+    $stbi->execute();
+    $res = $stbi->fetch();
+    //print_r($res);
+    if($res!=null){
+        if($res['ID_PERFIL']==1){
+            $_SESSION['userid']=$res['ID_PERFIL'];
+            $_SESSION['pswd']=$res['PASW_USER'];
+            echo "1";
+        }elseif ($res['ID_PERFIL']==3){
+            $_SESSION['userid']=$res['ID_PERFIL'];
+            $_SESSION['pswd']=$res['PASW_USER'];
+            echo "3";
         }
-	} else {
-        echo false;
-    }
-} catch (Exception $e) {
+    }else {echo "4";}
+} catch (PDOException $e) {
     echo 'Message: '.$e->getMessage();
 }
 
